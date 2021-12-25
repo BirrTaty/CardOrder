0411e1ea4708cfde8f8f1ccf11104c8ad1c5d8ea
Tatiana <birrtaty@gmail.com>
2021-12-25 21:43:55 +0300

    Final version

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,21 +1,19 @@
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 
     @Test
-    void shouldSendCorrectMessage(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Татьяна");
-        form.$("[data-test-id=phone] input").setValue("+79218671153");
-        form.$("[data-test-id=agreement]").click();
-        form.$("button").click();
-        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время!"));
+    void shouldTest() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Иванова Анна");
+        $("[data-test-id=phone] input").setValue("+79291234567");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
