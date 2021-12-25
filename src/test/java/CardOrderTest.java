3a5a647ab0720f4387f2190e300e157b15b5c939
Tatiana <birrtaty@gmail.com>
2021-12-25 21:55:13 +0300

    Final version with all tests

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -7,11 +7,31 @@ import static com.codeborne.selenide.Selenide.open;
 public class CardOrderTest {
 
     @Test
-    void shouldTest() {
+    void shouldPathWithSpaceInName() {
         open("http://localhost:9999");
 
-        $("[data-test-id=name] input").setValue("Иванова Анна");
-        $("[data-test-id=phone] input").setValue("+79291234567");
+        $("[data-test-id=name] input").setValue("Бирюкова Татьяна");
+        $("[data-test-id=phone] input").setValue("+79218671153");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldPathWithHyphenAndSpaceInName() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Бирюкова Татьяна-Виктория");
+        $("[data-test-id=phone] input").setValue("+79218671153");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldPathWithOneLetter() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Б");
+        $("[data-test-id=phone] input").setValue("+79218671153");
         $("[data-test-id=agreement]").click();
         $("button").click();
         $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
